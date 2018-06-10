8df210e465d62c38903bec7ba3f0e0a1e6bc28f5
NastyaLo <loyko2802@gmail.com>
2018-06-10 22:53:23 +0300

    Ввод пин-кода с экрана.

--- pin_pad/main.js
+++ pin_pad/main.js
@@ -1,107 +1,111 @@
-var one       = document.getElementsByClassName("screen__button--1"),
-    two       = document.getElementsByClassName("screen__button--2"),
-    three     = document.getElementsByClassName("screen__button--3"),
-    four      = document.getElementsByClassName("screen__button--4"),
-    five      = document.getElementsByClassName("screen__button--5"),
-    six       = document.getElementsByClassName("screen__button--6"),
-    seven     = document.getElementsByClassName("screen__button--7"),
-    eight     = document.getElementsByClassName("screen__button--8"),
-    nine      = document.getElementsByClassName("screen__button--9"),
-    zero      = document.getElementsByClassName("screen__button--0"),
-    fix       = document.getElementsByClassName("screen__button__arrow");
+var one       = document.getElementById("screen__button--1"),
+    two       = document.getElementById("screen__button--2"),
+    three     = document.getElementById("screen__button--3"),
+    four      = document.getElementById("screen__button--4"),
+    five      = document.getElementById("screen__button--5"),
+    six       = document.getElementById("screen__button--6"),
+    seven     = document.getElementById("screen__button--7"),
+    eight     = document.getElementById("screen__button--8"),
+    nine      = document.getElementById("screen__button--9"),
+    zero      = document.getElementById("screen__button--0"),
+    fix       = document.getElementById("screen__button__arrow");
 
-var cell_1      = document.getElementsByClassName("screen__cell--1"),
-    cell_2      = document.getElementsByClassName("screen__cell--2"),
-    cell_3      = document.getElementsByClassName("screen__cell--3"),
-    cell_4      = document.getElementsByClassName("screen__cell--4");
+var cell_1      = document.getElementById("screen__cell--1"),
+    cell_2      = document.getElementById("screen__cell--2"),
+    cell_3      = document.getElementById("screen__cell--3"),
+    cell_4      = document.getElementById("screen__cell--4");
 
 var screenInput = [];
-console.log(screenInput.lenght);
+console.log(screenInput.length);
 
 one.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(1);
-    console.log("1");
   }
-} 
+}
 
 two.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(2);
   }
 }
 
 three.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(3);
   }
 }
 
 four.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(4);
   }
 }
 
 five.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(5);
   }
 }
 
 six.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(6);
   }
 }
 
 seven.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(7);
   }
 }
 
 eight.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(8);
   }
 }
 
 nine.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(9);
   }
 }
 
 zero.onclick = function () {
-  if (screenInput.lenght < 4) {
+  if (screenInput.length < 4) {
     screenInput.push(0);
   }
 }
 
 fix.onclick = function () {
-  if (screenInput.lenght > 0) {
-    screenInput.pop;
+  if (screenInput.length > 0) {
+    screenInput.pop();
   }
 }
 
-if (screenInput.lenght != 0) {
-  setInterval (function() {
-    if (screenInput[0] > 0) {
-      cell_1.innerHTML = screenInput[0];
-    }
+setInterval(function() {
+  if (screenInput[0] > 0 && screenInput[0] != undefined) {
+    cell_1.innerHTML = screenInput[0];
+  } else if (screenInput[0] == undefined) {
+    cell_1.innerHTML = " ";
+  }
 
-    if (screenInput[1] > 0) {
-      cell_2.innerHTML = screenInput[1];
-    }
+  if (screenInput[1] > 0 && screenInput[1] != undefined) {
+    cell_2.innerHTML = screenInput[1];
+  } else if (screenInput[1] == undefined) {
+    cell_2.innerHTML = " ";
+  }
 
-    if (screenInput[2] > 0) {
-      cell_3.innerHTML = screenInput[3];
-    }
+  if (screenInput[2] > 0 && screenInput[2] != undefined) {
+    cell_3.innerHTML = screenInput[2];
+  }  else if (screenInput[2] == undefined) {
+    cell_3.innerHTML = " ";
+  }
 
-    if (screenInput[3] > 0) {
-      cell_4.innerHTML = screenInput[4];
-    }
-
-  }, 1000);
-}
+  if (screenInput[3] > 0 && screenInput[3] != undefined) {
+    cell_4.innerHTML = screenInput[3];
+  } else if (screenInput[3] == undefined) {
+    cell_4.innerHTML = " ";
+  }
+}, 1);
